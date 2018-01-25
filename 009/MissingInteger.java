b34536637df2d61843eaff45416982bff3e294ef
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-01-26 00:43:18 +0100

    even worse

--- 009/MissingInteger.java
+++ 009/MissingInteger.java
@@ -2,19 +2,23 @@ import java.util.Arrays;
 
 public class Solution {
 	public int solution(int[] A) { // 81%
+		int B [] = new int[A.length];
 
 		if (A.length == 1) {
-			return 1;
+			return A[0]>0?A[0]+1:1;
 		}
 
-		Arrays.sort(A);
+		for (int i=0;i< A.length;i++)
+			if (A[i] > 0)
+				B[i] = A[i];
 
-		for (int i = 1; i < A.length; i++) {
-			if (A[i-1] <0) continue;
-			if (A[i-1] != A[i] && A[i - 1] != A[i] - 1)
-				return A[i]-1;
-		}
-		return (A[A.length-1] > 0)?A[A.length-1] +1:1;
+		Arrays.sort(B);
+
+		for (int i = 1; i < B.length; i++)
+			if (B[i-1] != B[i] && B[i - 1] != B[i] - 1)
+				return B[i]-1;
+
+		return (B[B.length-1] == 0)?1:(B[B.length-1]+1);
 	}
 
 	public static void main(String[] args) {
@@ -26,5 +30,6 @@ public class Solution {
 		System.out.println("\n:" + s.solution(new int[] { 1, 3, 1, 3, 2, 1, 5 }));
 		System.out.println("\n:" + s.solution(new int[] { 3, 1, 2, 4, 3 }));
 		System.out.println("\n:" + s.solution(new int[] { -1, -3}));
+		System.out.println("\n:" + s.solution(new int[] {-3,-5,-7,-1,0,3,5}));
 	}
 }
