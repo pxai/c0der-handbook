cd3bc1b80e67d465c203d91da6bd57fcec313e89
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-01-26 00:54:23 +0100

    even worse

--- 009/MissingInteger.java
+++ 009/MissingInteger.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
 
-public class Solution {
-	public int solution(int[] A) { // 81%
-		int B [] = new int[A.length];
+class Solution {
+    public int solution(int[] A) {
+    	int B [] = new int[A.length];
 
 		if (A.length == 1) {
-			return A[0]>0?A[0]+1:1;
+			return (A[0]<=0)?1:(A[0]>1?1:2);
 		}
 
 		for (int i=0;i< A.length;i++)
@@ -13,20 +15,21 @@ public class Solution {
 				B[i] = A[i];
 
 		Arrays.sort(B);
-
+		//for (int i: B)	{ System.out.print(i +",");}
+		if (B[0] > 1) { return 1; }
 		for (int i = 1; i < B.length; i++)
 			if (B[i-1] != B[i] && B[i - 1] != B[i] - 1)
 				return B[i]-1;
 
 		return (B[B.length-1] == 0)?1:(B[B.length-1]+1);
-	}
+    }
 
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
 		System.out.println("\n:" + s.solution(new int[] { 1 }));
 		System.out.println("\n:" + s.solution(new int[] { 0 }));
-		System.out.println("\n:" + s.solution(new int[] { 2 }));
+		System.out.println("\n:" + s.solution(new int[] { 4, 5, 6, 2 }));
 		System.out.println("\n:" + s.solution(new int[] { 1, 3, 1, 3, 2, 1, 5 }));
 		System.out.println("\n:" + s.solution(new int[] { 3, 1, 2, 4, 3 }));
 		System.out.println("\n:" + s.solution(new int[] { -1, -3}));
