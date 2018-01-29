96dd5184df2686ded039d00e6826b33e7e2cfdbc
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-01-29 23:48:13 +0100

    ok, done

--- 014/GnomicRangeQuery.java
+++ 014/GnomicRangeQuery.java
@@ -1,10 +1,10 @@
-
 import java.util.Hashtable;
 import java.util.Arrays;
 
-class GnomicRangeQuery {
-	public int[] solution(String S, int[] P, int[] Q) {
+class Solution {
+	public int[] solution(String S, int[] P, int[] Q) { // 62% , obvious result
     	    int sol[] = new int[P.length];
+    	    int gen[] = new int[S.length()];
 
     	    Hashtable<String, Integer> hash = new Hashtable();
     	    hash.put("A",1);
@@ -12,11 +12,17 @@ class GnomicRangeQuery {
     	    hash.put("G",3);
     	    hash.put("T",4);
 
+    	    for (int i= 0; i< gen.length; i++)
+    	    	gen[i] = hash.get(S.charAt(i)+"");
+
     	    for (int i=0;i<P.length;i++) {
-    	    	char[] sub = S.substring(P[i], Q[i]+1).toCharArray();
-    	    	Arrays.sort(sub);
-    	    	System.out.println(sub);
-    	    	sol[i] = hash.get(sub[0]+"");
+    	    	//char[] sub = S.substring(P[i], Q[i]+1).toCharArray();
+    	    	int min = 5;
+    	    	for (int j=P[i]; j <= Q[i];j++) {
+    	    		if (gen[j] == 1) { min = 1; break;}
+    	    		else if (gen[j]<min) { min = gen[j];}
+    	    	}
+    	    	sol[i] = min;
     	    }
 
     	    return sol;
