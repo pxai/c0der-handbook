e4f4416d14a181fdc13ff5d813f722286f1a67d1
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-01-30 00:50:14 +0100

    ok, done

--- 014/GnomicRangeQuery.java
+++ 014/GnomicRangeQuery.java
@@ -1,36 +1,80 @@
+
 import java.util.Hashtable;
+import java.util.stream.Collectors;
 import java.util.Arrays;
 
 class Solution {
-	public int[] solution(String S, int[] P, int[] Q) { // 62% , obvious result
-    	    int sol[] = new int[P.length];
-    	    int gen[] = new int[S.length()];
+	public int[] solution(String S, int[] P, int[] Q) { // 100%
+		int sol[] = new int[P.length];
+		int gen[] = new int[S.length()];
+		int A[] = new int[S.length()];
+		int C[] = new int[S.length()];
+		int G[] = new int[S.length()];
+		int T[] = new int[S.length()];
+		int a=0, c=0,g=0,t = 0;
 
-    	    Hashtable<String, Integer> hash = new Hashtable();
-    	    hash.put("A",1);
-    	    hash.put("C",2);
-    	    hash.put("G",3);
-    	    hash.put("T",4);
+		Hashtable<String, Integer> hash = new Hashtable();
+		hash.put("A", 1);
+		hash.put("C", 2);
+		hash.put("G", 3);
+		hash.put("T", 4);
 
-    	    for (int i= 0; i< gen.length; i++)
-    	    	gen[i] = hash.get(S.charAt(i)+"");
+		for (int i = gen.length - 1; i >= 0; i--) {
+			gen[i] = hash.get(S.charAt(i) + "");
 
-    	    for (int i=0;i<P.length;i++) {
-    	    	//char[] sub = S.substring(P[i], Q[i]+1).toCharArray();
-    	    	int min = 5;
-    	    	for (int j=P[i]; j <= Q[i];j++) {
-    	    		if (gen[j] == 1) { min = 1; break;}
-    	    		else if (gen[j]<min) { min = gen[j];}
-    	    	}
-    	    	sol[i] = min;
-    	    }
+			switch (gen[i]) {
+			case 1:
+				A[i] = a = 1;
+				C[i] = (c!=0)?++c:0;
+				G[i] = (g!=0)?++g:0;
+				T[i] = (t!=0)?++t:0;
+				break;
+			case 2:
+				C[i] = c = 1;
+				A[i] = (a!=0)?++a:0;
+				G[i] = (g!=0)?++g:0;
+				T[i] = (t!=0)?++t:0;
+				break;
+			case 3:
+				G[i] = g = 1;
+				A[i] = (a!=0)?++a:0;
+				C[i] = (c!=0)?++c:0;
+				T[i] = (t!=0)?++t:0;
+				break;
+			case 4:
+				T[i] = t = 1;
+				A[i] = (a!=0)?++a:0;
+				C[i] = (c!=0)?++c:0;
+				G[i] = (g!=0)?++g:0;
+				break;
+			default:
+				break;
+			}
+		}
 
-    	    return sol;
-    }
+		System.out.println(Arrays.toString(A));
+		System.out.println(Arrays.toString(C));
+		System.out.println(Arrays.toString(G));
+		System.out.println(Arrays.toString(T));
+
+		for (int i = 0; i < P.length; i++) {
+			// char[] sub = S.substring(P[i], Q[i]+1).toCharArray();
+			int min = 5;
+
+			if (Q[i] - P[i] >= (T[P[i]] - 1) && (T[P[i]] - 1)>=0) { min = 4; }
+			if (Q[i] - P[i] >= (G[P[i]] - 1)&& (G[P[i]] - 1)>=0) { min = 3; }
+			if (Q[i] - P[i] >= (C[P[i]] - 1)&& (C[P[i]] - 1)>=0) { min = 2; }
+			if (Q[i] - P[i] >= (A[P[i]] - 1)&& (A[P[i]] - 1)>=0) { min = 1; }
+
+			sol[i] = min;
+		}
+
+		return sol;
+	}
 
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
-		System.out.println(s.solution("CAGCCTA", new int[] { 2,5,0}, new int[] { 4,5,6}));
+		System.out.println(Arrays.toString(s.solution("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 })));
 	}
 }
