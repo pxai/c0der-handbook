7e0505744240d12a05b5d250d405ec703ab519a3
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-02-06 00:01:53 +0100

    ok

--- 021/Fish.java
+++ 021/Fish.java
@@ -4,9 +4,32 @@ import java.util.Stack;
 // System.out.println("this is a debug message");
 
 class Fish {
-    public int solution(int[] A, int[] B) {
+  public int solution(int[] A, int[] B) {
       int solution = 0;
+      int v = 0;
+
+      Stack<Integer> fishes = new Stack<Integer>();
+      Stack<Integer> killers = new Stack<Integer>();
+
+      int pushThem = -1;
+
+      for (int i = 0;i < A.length;i++) {
+          if (B[i] != 0) {
+              if (pushThem == -1) { pushThem = i; }
+              killers.push(B[i]);
+          }
+
+          if (pushThem != -1) {
+              fishes.push(A[i]);
+          }
+      }
+
+
+
+      for (int i = pushThem;i<B.length;i++) {
+
+      }
 
       return solution;
-    }
+  }
 }
