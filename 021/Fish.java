0e422cf7cbb59f0efd28927ece4190c8edf5d267
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-02-06 08:54:18 +0100

    ok,

--- 021/Fish.java
+++ 021/Fish.java
@@ -1,5 +1,6 @@
 import java.util.Stack;
 
+
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
@@ -10,7 +11,7 @@ class Fish {
 
       Stack<Integer> fishes = new Stack<Integer>();
       Stack<Integer> killers = new Stack<Integer>();
-
+      int survivors = 0;
       int pushThem = -1;
 
       for (int i = 0;i < A.length;i++) {
@@ -23,13 +24,29 @@ class Fish {
               fishes.push(A[i]);
           }
       }
+//
+//      System.out.println(fishes);
+//      System.out.println(killers);
 
-
-
-      for (int i = pushThem;i<B.length;i++) {
-
+      for (int i = pushThem;i<A.length;i++) {
+    	  if (killers.isEmpty()) { break; }
+    	  int top = fishes.peek();
+    	  int killer = killers.peek();
+    	  if (top > killer) {
+    		  survivors++;
+    		  killers.pop();
+    		  fishes.pop();
+    	  } else if (killer > top) {
+    		  fishes.pop();
+    	  }
       }
 
-      return solution;
+      return pushThem + survivors +  (killers.isEmpty()?0:killers.size());
   }
+
+	public static void main(String[] args) {
+
+		Solution s = new Solution();
+		System.out.println(s.solution(new int[] {4,3,2,1,5}, new int[]{0,1,0,0,0}));
+	}
 }
