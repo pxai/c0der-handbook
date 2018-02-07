4187bc8523419c1f9f54394b89b4446efa1f8807
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-02-07 23:58:25 +0100

    ok

---
+++ 023/EquiLeader.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Stack;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class EquiLeader {
+    public int solution(String S) { // 100%
+
+    }
+}
