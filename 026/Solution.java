d57118953769b254eccbc1c1281994ea68a0f677
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-02-12 00:05:45 +0100

    ok

---
+++ 026/Solution.java
@@ -0,0 +1,33 @@
+/ System.out.println("this is a debug message");
+
+class Solution {
+  public int solution(int[] A) {
+    int [] B = new int[A.length];
+
+	  B[0] = A[0];
+	  for(int i=1;i<A.length;i++)
+		  B[i] = B[i-1] + A[i];
+
+	 //Arrays.asList(B).stream().map(Object::toString).collect(Collectors.joining(", "));
+	  int max = B[0];
+	  for(int i=1;i<B.length;i++) {
+				if (A[i]> B[i]) {
+			    B[i] = A[i];
+
+				}
+			    if (B[i] > max) {
+				max = B[i];}
+
+	  }
+
+	  return max;
+  }
+
+	public static void main(String[] args) {
+
+		Solution s = new Solution();
+		System.out.println(s.solution(new int[] {4,3,-5,2}));
+		System.out.println(s.solution( new int[]{0}));
+		System.out.println(s.solution(new int[] {4,3,-2,1,5}));
+	}
+}
