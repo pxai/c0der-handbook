e3ac4f77cf9e0d7e3d82d26e2367eb1370e6602f
pxai <pello_altadill@cuatrovientos.org>
2018-02-12 20:26:58 +0100

    ok ok

--- 025/MaxSliceSum.java
+++ 025/MaxSliceSum.java
@@ -1,26 +1,54 @@
 / System.out.println("this is a debug message");
 
 class Solution {
-  public int solution(int[] A) {
+  public int solution(int[] A) { // 84%
     int [] B = new int[A.length];
+    int [] C = new int[A.length];
 
-	  B[0] = A[0];
-	  for(int i=1;i<A.length;i++)
-		  B[i] = B[i-1] + A[i];
+    B[0] = A[0];
 
-	 //Arrays.asList(B).stream().map(Object::toString).collect(Collectors.joining(", "));
-	  int max = B[0];
-	  for(int i=1;i<B.length;i++) {
-				if (A[i]> B[i]) {
-			    B[i] = A[i];
 
-				}
-			    if (B[i] > max) {
-				max = B[i];}
+    for(int i=1;i<A.length;i++) {
+      B[i] = B[i-1] + A[i];
+    }
 
-	  }
+    C[A.length-1] = A[A.length-1];
+    for(int i=A.length-2;i>=0;i--) {
+        C[i] = C[i + 1] + A[i] ;
+    }
 
-	  return max;
+//		  System.out.print("\nB: ");
+//		  for (int i = 0; i < B.length; i++) {
+//				System.out.print(B[i] + ", ");
+//			}
+//		  System.out.print("\nC: ");
+//		  for (int i = 0; i < C.length; i++) {
+//				System.out.print(C[i] + ", ");
+//			}
+
+    //Arrays.asList(B).stream().map(Object::toString).collect(Collectors.joining(", "));
+
+    int max = B[0];
+    for(int i=1;i<B.length;i++) {
+        if (A[i]> B[i]) {
+           B[i] = A[i];
+        }
+          if (B[i] > max) {
+        max = B[i];}
+
+    }
+
+    int max2 = C[0];
+    for(int i=1;i<C.length;i++) {
+        if (A[i]> C[i]) {
+           C[i] = A[i];
+        }
+          if (C[i] > max2) {
+        max2 = C[i];}
+
+    }
+
+    return max>max2?max:max2;
   }
 
 	public static void main(String[] args) {
